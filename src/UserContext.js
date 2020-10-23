df99d65a0c3a7ac92eac12cefe82ddc982ca5383
Caio Augusto <caioamfr@gmail.com>
2020-10-23 13:26:40 -0300

    Problema ao Logar resolvido

--- src/UserContext.js
+++ src/UserContext.js
@@ -32,8 +32,9 @@ export const UserStorage = ({ children }) => {
     try {
       setError(null);
       setLoading(true);
-      const { url, options } = TOKEN_POST({ username, password });
+      const { url, options } = TOKEN_POST(username, password);
       const tokenRes = await fetch(url, options);
+      console.log(url, options);
       if (!tokenRes.ok) throw new Error(`Error: Usuário inválido.`);
       const { token } = await tokenRes.json();
       localStorage.setItem("token", token);
