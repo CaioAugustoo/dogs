7669b52313c23e97e077bb367754ccda74fa3a27
Caio Augusto <caioamfr@gmail.com>
2020-10-23 13:28:12 -0300

    Remoção de console.log

--- src/UserContext.js
+++ src/UserContext.js
@@ -34,7 +34,6 @@ export const UserStorage = ({ children }) => {
       setLoading(true);
       const { url, options } = TOKEN_POST(username, password);
       const tokenRes = await fetch(url, options);
-      console.log(url, options);
       if (!tokenRes.ok) throw new Error(`Error: Usuário inválido.`);
       const { token } = await tokenRes.json();
       localStorage.setItem("token", token);
