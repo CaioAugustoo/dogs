479a40f57309a24c41b0a36e0aa804374375e526
Caio Augusto <caioamfr@gmail.com>
2020-10-23 12:57:30 -0300

    Envio dos arquivos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import ProtectedRoute from "./Components/Helper/ProtectedRoute/ProtectedRoute";
+import Login from "./Components/Login/Login";
+import User from "./Components/User/User";
+import Header from "./Components/Header/Header";
+import { UserStorage } from "./UserContext";
+import GlobalStyle from "./style/GlobalStyle";
 
 const App = () => {
   return (
-    <h1>React App</h1>
+    <BrowserRouter>
+      <UserStorage>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login/*" element={<Login />} />
+          <ProtectedRoute path="conta/*" element={<User />} />
+        </Routes>
+      </UserStorage>
+      <GlobalStyle />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
