89e14856bf9480b0d6e20c0abdabb035f16aedfe
Caio Augusto <caioamfr@gmail.com>
2020-10-26 10:38:22 -0300

    Uso do Head

--- src/Components/User/UserProfile.js
+++ src/Components/User/UserProfile.js
@@ -3,12 +3,14 @@ import Feed from "../Feed/Feed";
 import { Container } from "../../style/GlobalStyle";
 import { useParams } from "react-router-dom";
 import Title from "../Title/Title";
+import Head from "../Helper/Head/Head";
 
 const UserProfile = () => {
   const { user } = useParams();
 
   return (
     <Container style={{ marginTop: "2rem" }}>
+      <Head title={user} />
       <Title sectionTitle={user} />
       <Feed user={user} />
     </Container>
