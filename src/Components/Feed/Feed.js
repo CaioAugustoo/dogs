3e670b48da72b93b7fdee6080b552b40a256a9a8
Caio Augusto <caioamfr@gmail.com>
2020-10-25 23:17:40 -0300

    Uso de PropTypes

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import FeedModal from "./FeedModal";
 import FeedPhotos from "./FeedPhotos";
+import PropTypes from "prop-types";
 
 const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = useState(null);
@@ -48,6 +49,17 @@ const Feed = ({ user }) => {
       ))}
     </div>
   );
+};
+
+Feed.defaultProps = {
+  user: 0,
+};
+
+Feed.propTypes = {
+  user: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.number.isRequired,
+  ]),
 };
 
 export default Feed;
