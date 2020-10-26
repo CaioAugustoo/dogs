37f042a5745a7ab1210c7ef1c4c92b4976a3f2f5
Caio Augusto <caioamfr@gmail.com>
2020-10-26 10:56:31 -0300

    Footer finalizado

--- src/Components/Footer/style.js
+++ src/Components/Footer/style.js
@@ -1,5 +1,14 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const FooterStyled = styled.footer`
-  height: 5rem
+  height: 5rem;
+  background: #11a3ff;
+  padding: 3rem 1rem 0 1rem;
+  height: 10rem;
+  text-align: center;
+  color: white;
+
+  p {
+    margin-top: 1rem;
+  }
 `;
