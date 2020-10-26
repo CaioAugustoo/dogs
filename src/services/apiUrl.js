8bff2f46ba19a4ec843f2ce69ca647e07699fc50
Caio Augusto <caioamfr@gmail.com>
2020-10-26 10:05:06 -0300

    Função para resetar senha

--- src/services/apiUrl.js
+++ src/services/apiUrl.js
@@ -123,3 +123,16 @@ export const PASSWORD_LOST = body => {
     },
   };
 };
+
+export const PASSWORD_REST = body => {
+  return {
+    url: API_URL + "/api/password/reset",
+    options: {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    },
+  };
+};
