394dcee14f7797c3c8bd3892ddc61982c57226bf
Caio Augusto <caioamfr@gmail.com>
2020-10-26 10:35:27 -0300

    Head atualizado

--- src/Components/Helper/Head/Head.js
+++ src/Components/Helper/Head/Head.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 
 const Head = props => {
   useEffect(() => {
-    document.title = props.title + '| Dogs';
+    document.title = props.title + ' | Dogs';
     document
       .querySelector("meta[name='description']")
       .setAttribute("content", props.description || '');
