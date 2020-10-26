6a27dd0419de68e7678b9e4967817bfb1750e0e1
Caio Augusto <caioamfr@gmail.com>
2020-10-25 22:21:37 -0300

    Alterada quantidade de fotos no feed

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -11,7 +11,7 @@ const FeedPhotos = ({ setInfinite, page, user, setModalPhoto }) => {
   const { data, loading, error, request } = useFetch();
   useEffect(() => {
     const fetchPhotos = async () => {
-      const total = 3;
+      const total = 6;
       const { url, options } = PHOTOS_GET({ page, total, user });
       const { response, json } = await request(url, options);
       if (response && response.ok && json.length < total) setInfinite(false);
