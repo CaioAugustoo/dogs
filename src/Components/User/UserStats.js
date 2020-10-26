68028538dfae6ca222ec73ec93538083b9ad8087
Caio Augusto <caioamfr@gmail.com>
2020-10-26 11:21:11 -0300

    UserStats finalizado

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,12 +1,34 @@
 import React from "react";
 import Head from "../Helper/Head/Head";
+import useFetch from "../../Hooks/useFetch";
+import { useEffect } from "react";
+import Loading from "../Helper/Loading/Loading";
+import { Error } from "../Inputs/style";
+import { GET_STATS } from "../../services/apiUrl";
+import UserStatsGraphs from "./UserStatsGraphs";
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(() => {
+    const getData = async () => {
+      const { url, options } = GET_STATS();
+
+      await request(url, options);
+    };
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error>{error}</Error>;
+  if (data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
