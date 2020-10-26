acea3225c718f87597d7240442483c2053e99336
Caio Augusto <caioamfr@gmail.com>
2020-10-25 22:40:04 -0300

    Criado componente para visualização de uma única imagem

---
+++ src/Components/Photo/Photo.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { PHOTO_GET } from "../../services/apiUrl";
+import { useParams } from "react-router-dom";
+import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error/Error";
+import Loading from "../Helper/Loading/Loading";
+import PhotoContent from "./PhotoContent";
+import { Container } from "../../style/GlobalStyle";
+
+const Photo = () => {
+  const { id } = useParams();
+
+  const { data, loading, error, request } = useFetch();
+
+  useEffect(() => {
+    const { url, options } = PHOTO_GET(id);
+    request(url, options);
+  }, [id, request]);
+
+  if (error) return <Error erroMessage={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <Container style={{ marginTop: "2rem" }}>
+        <PhotoContent single={true} data={data} />
+      </Container>
+    );
+  else return null;
+};
+
+export default Photo;
