5c9173a173f7b3e10696ea7cb6a232fab197a7b2
Caio Augusto <caioamfr@gmail.com>
2020-10-26 09:56:28 -0300

    Remoção da desestruturação

--- src/Components/Login/LoginPasswordLost.js
+++ src/Components/Login/LoginPasswordLost.js
@@ -18,7 +18,7 @@ const LoginPasswordLost = () => {
         login: login.value,
         url: window.location.href.replace("perdeu", "resetar"),
       });
-      const { json } = await request(url, options);
+      await request(url, options);
     }
   };
 
