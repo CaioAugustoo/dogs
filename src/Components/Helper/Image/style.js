1da952df173dba96fccde68c42017bcc98121399
Caio Augusto <caioamfr@gmail.com>
2020-10-29 07:24:37 -0300

    Estilo do skeleton atualizado

--- src/Components/Helper/Image/style.js
+++ src/Components/Helper/Image/style.js
@@ -1,5 +1,30 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-export const Wrapper = styled.div``;
+const skeleton = keyframes`
+  from {
+    background-position: 0rem;
+  } to {
+    background-position: -200%;
+  }
+`;
 
-export const Skeleton = styled.div``;
+export const Wrapper = styled.div`
+  display: grid;
+
+  img {
+    display: block;
+    max-width: 100%;
+    grid-area: 1/1;
+    opacity: 0;
+    transition: 0.2s;
+  }
+`;
+
+export const Skeleton = styled.div`
+  grid-area: 1/1;
+  height: 100%;
+  background-image: linear-gradient(90deg, #eee 0rem, #fff 50%, #eee 100%);
+  background-color: #eee;
+  background-size: 200%;
+  animation: ${skeleton} 1.5s infinite linear;
+`;
